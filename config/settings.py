d380add7665fc46fd17b95d917c2dfcde4b5c1ce
supr-inego <cyrildayak03@gmail.com>
2026-02-23 22:31:46 +0800

    Connected to PostgreSQL

--- config/settings.py
+++ config/settings.py
@@ -48,9 +48,13 @@ TEMPLATES = [
 WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ordering_system',
+        'USER': 'postgres',
+        'PASSWORD': 'Cyrilgwapo12',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
